3d85f84ebe71a915d97de20739227850c7f52128
flemming chow <flemming@flemmings-MacBook-Pro.local>
2021-03-08 20:24:11 -0800

    cleaned up asset imports

---
+++ src/assets/projects/index.ts
@@ -0,0 +1,6 @@
+import LoveYourPlanet from "./LoveYourPlanet.png";
+import Clio from "./clio.png";
+import MovieFinder from "./movieFinderUI.png";
+import TaiwanAssociation from "./TwnAssociation.png";
+
+export { LoveYourPlanet, Clio, MovieFinder, TaiwanAssociation };
